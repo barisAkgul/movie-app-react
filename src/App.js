7aad83b19492114bee583da1dcdd977f95074ba4
barisAkgul <barisakgul1994@gmail.com>
2023-02-17 23:39:11 +0300

    Router and layouts folders added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import Test from "components/Test";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//styles
+import "styles/reset.css";
+import "styles/variables.css";
+import "styles/global.css";
+
+import { HomePage } from "pages/home-page";
 
 function App() {
   return (
-    <>
-      <Test />
-    </>
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/hakkımızda",
+          element: <div>Hakkımızda</div>,
+        },
+      ])}
+    />
   );
 }
 
---
+++ src/pages/home-page/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Components
+import { HomePageContainer } from "containers/home-page-container";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HomePageContainer />
+    </div>
+  );
+};
+
+export { HomePage };
